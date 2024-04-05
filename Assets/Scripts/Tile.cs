f9eaac0ea4ebfc6ea48d0ed5fd0c36635dfaabc8
YamYam2E <32175094+YamYam2E@users.noreply.github.com>
2024-04-06 01:50:56 +0900

    Add Tile object to save tile information

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Tile
+{
+    public Vector2Int Index;
+    public Vector2 Direction;
+    public Vector2 Position;
+    public int Distance;
+    public bool IsBlock;
+}
--- Assets/Scripts/VectorField2D.cs
+++ Assets/Scripts/VectorField2D.cs
@@ -4,15 +4,16 @@ using UnityEngine.Tilemaps;
 
 public partial class VectorField2D : MonoBehaviour
 {
-    [Header("For Debugging")]
-    [SerializeField] private bool isShowGroundArea;
+    [Header("For Debugging")] [SerializeField]
+    private bool isShowGroundArea;
+
     [SerializeField] private bool isShowObstacleArea;
-    
-    [Space]
+
+    [Space] 
     [SerializeField] private Vector2Int mapSize;
     [SerializeField] private Transform goalTransform;
 
-    [Space]
+    [Space] 
     [SerializeField] private Tilemap groundTilemap;
     [SerializeField] private Tilemap obstacleTilemap;
 
@@ -20,14 +21,15 @@ public partial class VectorField2D : MonoBehaviour
     private Vector3Int fromGround, fromObstacle, toGround, toObstacle;
     private List<Vector2> obstaclePositions = new();
     private List<Vector2> groundPositions = new();
+    private Vector3Int goalPosition;
+    
+    private Tile[,] fields;
     
     private void Start()
     {
         Initialize();
-        SetObstaclePositions();
-        SetGroundPositions();
-        
         UpdateGoalPosition();
+        CreateFields();
     }
 
     private void Initialize()
@@ -35,9 +37,12 @@ public partial class VectorField2D : MonoBehaviour
         tileWorldOffset = new Vector2(0.5f, 0.5f);
         fromGround = groundTilemap.origin;
         toGround = groundTilemap.size + groundTilemap.origin;
-        
+
         fromObstacle = obstacleTilemap.origin;
         toObstacle = obstacleTilemap.size + obstacleTilemap.origin;
+
+        var size = groundTilemap.size;
+        fields = new Tile[size.x, size.y];
     }
 
     private void OnDrawGizmos()
@@ -52,13 +57,13 @@ public partial class VectorField2D : MonoBehaviour
         if (isShowObstacleArea)
         {
             Gizmos.color = new Color(1f, .2f, .2f, 0.3f);
-        
+
             obstaclePositions.ForEach(
                 position =>
                 {
                     //타일 사이즈가 1x1 일 경우에, 중앙으로 위치하기 위해서 0.5 씩 위치를 더해준다.
                     Gizmos.DrawCube(
-                        new Vector3(position.x + 0.5f, position.y + 0.5f),
+                        position,
                         Vector3.one);
                 });
         }
@@ -66,13 +71,13 @@ public partial class VectorField2D : MonoBehaviour
         if (isShowGroundArea)
         {
             Gizmos.color = new Color(0.5f, 0.8f, 0.6f, 0.2f);
-        
+
             groundPositions.ForEach(
                 position =>
                 {
                     //타일 사이즈가 1x1 일 경우에, 중앙으로 위치하기 위해서 0.5 씩 위치를 더해준다.
                     Gizmos.DrawWireCube(
-                        new Vector3(position.x + 0.5f, position.y + 0.5f),
+                        position,
                         Vector3.one);
                 });
         }
@@ -88,13 +93,13 @@ public partial class VectorField2D : MonoBehaviour
             for (var y = fromObstacle.y; y < toObstacle.y; ++y)
             {
                 var tileWorldPosition = obstacleTilemap.transform.position + new Vector3(x, y);
-                var tileIndex = obstacleTilemap.WorldToCell( tileWorldPosition );
+                var tileIndex = obstacleTilemap.WorldToCell(tileWorldPosition);
                 var tile = obstacleTilemap.GetTile(tileIndex);
 
                 if (tile == null)
                     continue;
-                
-                obstaclePositions.Add(tileWorldPosition);
+
+                obstaclePositions.Add((Vector2)tileWorldPosition + tileWorldOffset);
             }
         }
     }
@@ -109,25 +114,65 @@ public partial class VectorField2D : MonoBehaviour
             for (var y = fromGround.y; y < toGround.y; ++y)
             {
                 var tileWorldPosition = groundTilemap.transform.position + new Vector3(x, y);
-                var tileIndex = groundTilemap.WorldToCell( tileWorldPosition );
+                var tileIndex = groundTilemap.WorldToCell(tileWorldPosition);
                 var tile = groundTilemap.GetTile(tileIndex);
 
                 if (tile == null)
                     continue;
                 
-                groundPositions.Add(tileWorldPosition);
+                groundPositions.Add((Vector2)tileWorldPosition + tileWorldOffset);
             }
         }
     }
 }
 
+
 public partial class VectorField2D
 {
+    /// <summary>
+    /// 필드의 가중치를 적용하기 위해 TileMap 기준 인덱스 및 포지션 값을 저장한 배열을 만든다.
+    /// </summary>
+    private void CreateFields()
+    {
+        var fieldX = 0;
+        var fieldY = 0;
+        
+        for (var x = fromGround.x; x < toGround.x; ++x)
+        {
+            for (var y = fromGround.y; y < toGround.y; ++y)
+            {
+                // * Ground
+                var tileWorldPosition = groundTilemap.transform.position + new Vector3(x, y);
+                var tileIndex = groundTilemap.WorldToCell(tileWorldPosition);
+                var groundTile = groundTilemap.GetTile(tileIndex);
+                var obstacleTile = obstacleTilemap.GetTile(tileIndex);
+                
+                if (groundTile == null)
+                    continue;
+                
+                fields[fieldX, fieldY] = new Tile
+                {
+                    Index = new Vector2Int(fieldX, fieldY),
+                    Position = (Vector2)tileWorldPosition + tileWorldOffset,
+                    Direction = Vector2.zero,
+                    Distance = -1,
+                    IsBlock = obstacleTile != null
+                };
+                
+                fieldY += 1;
+            }
+
+            fieldX += 1;
+            fieldY = 0;
+        }
+    }
+
     private void UpdateGoalPosition()
     {
-        var goalWorldPosition = goalTransform.position;
-        var position = groundTilemap.WorldToCell(groundTilemap.transform.position + goalWorldPosition);
-        
-        Debug.Log(position);
+        var position = groundTilemap.WorldToCell(groundTilemap.transform.position + goalTransform.position);
+
+        // Debug.Log(position + (Vector3)tileWorldOffset);
+
+        goalPosition = position;
     }
 }
